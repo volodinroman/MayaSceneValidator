b69cf1dd5156febcb5dd4b6a0b43bc34c9fb1a32
Roman Volodin <volodinroman1988@gmail.com>
2021-06-18 00:10:13 -0700

    resources and config classes

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-
+import os
+from MayaSceneValidator.core.resources import Resources
+root_ = os.path.dirname(__file__)
 
 class Validator(object):
     def __init__(self):
-        pass
+        self.resources = Resources()
+
+
 
 
 def main():
     v = Validator()
+
+
+if __name__ == '__main__':
+    main()
